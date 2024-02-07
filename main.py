ef2d59a2cb33340aec3d4707303f3583c621f107
AlexanderApollGray <90724200+AlexanderApollGray@users.noreply.github.com>
2024-02-07 10:00:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,31 @@ screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("Draw")
 
+BLACK = (0, 0, 0)
 WHITE = ( 255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 
-# Draw Function
-def drawRect():
-    pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
-    #pygame.draw.rect(screen, RED, [55, 50, 20, 25]): 
+class Stickman:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    #   self.vx = 0
+    #   self.vy = 0
+    #   self.speed = 5
+
+    # Draw Function
+    def drawStickman():
+        pygame.draw.circle(screen, BLACK, (self.x, self.y), 20)
+        pygame.draw.line(screen, BLACK, (self.x, self.y + 20), (self.x, self.y + 50), 3)
+        pygame.draw.line(screen, BLACK, (self.x, self.y + 30), (self.x - 10, self.y + 20), 3)
+        pygame.draw.line(screen, BLACK, (self.x, self.y + 30), (self.x + 10, self.y + 20), 3)
+        pygame.draw.line(screen, BLACK, (self.x, self.y + 50), (self.x - 10, self.y + 70), 3)
+        pygame.draw.line(screen, BLACK, (self.x, self.y + 50), (self.x + 10, self.y + 70), 3)
+        
+
+        #pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
+        #pygame.draw.rect(screen, RED, [55, 50, 20, 25],2)
 
 # Loop until the user clicks the close button.
 done = False
@@ -33,6 +50,7 @@ while not done:
 
     # Drawing
     screen.fill(WHITE)
+    drawStickman("RED", 100, 100)
 
     pygame.display.flip()
 
